750a6a9560ee58193ebbf49ee248b8f766e6dba6
ErickdeSouza567 <erickdesouza567@gmail.com>
2024-11-20 14:33:21 -0300

    Initial commit with models and connection strings for the database

---
+++ LojaMicro/LojaMicro.ApiProduto/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LojaMicro.ApiProduto.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LojaMicro.ApiProduto.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Product>? Products { get; set; }
+
+    }
+}
